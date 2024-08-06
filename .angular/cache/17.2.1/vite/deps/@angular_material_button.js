85645533946bb9109e5ef464853ed01cbed95b94
VicMac003 <vhmedinac03@gmail.com>
2024-08-05 20:40:25 -0600

    first commit

---
+++ .angular/cache/17.2.1/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-SY7MK7LB.js";
+import "./chunk-HSSSBUEI.js";
+import "./chunk-U7IY7LQI.js";
+import "./chunk-U4V3MJKI.js";
+import "./chunk-WSA2QMXP.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
